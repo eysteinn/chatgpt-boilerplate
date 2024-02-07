9b17321cb268803df6735177e28ace8abc365cfc
Eysteinn <eysteinnsig@gmail.com>
2024-02-07 23:04:16 +0000

    Working test

--- src/test5.py
+++ src/test5.py
@@ -2,18 +2,54 @@ from langchain.agents import AgentExecutor, create_react_agent, Tool
 from langchain_openai import ChatOpenAI
 #from langchain.vectorstores import Chroma
 from langchain_community.vectorstores import Chroma
-
+from langchain.chains import RetrievalQAWithSourcesChain
+#from langchain_community.embeddings import OpenAIEmbeddings
+from langchain_openai import OpenAIEmbeddings
 import os
 
+from langchain_community.document_loaders import TextLoader
+from langchain.text_splitter import RecursiveCharacterTextSplitter
 
-vectorstore = Chroma(persist_directory="./chroma_db")
+text = TextLoader(
+    "./state_of_the_union_2023.txt"
+).load()
 
+docs = RecursiveCharacterTextSplitter(
+    chunk_size=1000,
+    chunk_overlap=0,
+).split_documents(text)
+
+vectorstore = Chroma.from_documents(
+    documents=docs,
+    embedding=OpenAIEmbeddings(), persist_directory="./chroma_db"
+)
+vectorstore = Chroma(persist_directory="./chroma_db", embedding_function=OpenAIEmbeddings())
+qa_chain = RetrievalQAWithSourcesChain.from_chain_type(
+    llm=ChatOpenAI(temperature=0, model_name="gpt-3.5-turbo"),
+    chain_type="stuff",
+    retriever=vectorstore.as_retriever(),
+)
 def run_qa_chain(question):
-    print('Hallo')
-    results = qa_chain({"question":question},return_only_outputs=True)
+    print("Inside vector chain")
+    results = qa_chain.invoke({"question":question},return_only_outputs=True)
     return str(results)
 
 
+from langchain.tools import BaseTool
+from math import pi
+from typing import Union
+class CircumferenceTool(BaseTool):
+    name = "Circumference calculator"
+    description = "use this tool when you need to calculate a circumference using the radius of a circle, pass only the radius and not text and no units"
+
+    def _run(self, radius: Union[int, float]):
+        print("inside Circtool", radius)
+        
+        return float(radius)*2.0*pi
+
+    def _arun(self, radius: int):
+        raise NotImplementedError("This tool does not support async")
+    
 tool = Tool(
             name="State of the Union 2023 QA System",
             func=run_qa_chain,
@@ -22,7 +58,7 @@ tool = Tool(
                         "should be a fully formed question.",
             return_direct=True,
         )
-tools = [tool]
+tools = [tool, CircumferenceTool()]
 
 llm = ChatOpenAI(
         openai_api_key=os.environ["OPENAI_API_KEY"],
@@ -42,4 +78,5 @@ conversational_memory = ConversationBufferWindowMemory(
 agent = create_react_agent(llm, tools, prompt)
 agent_executor = AgentExecutor(agent=agent, tools=tools, verbose=True, memory = conversational_memory)
 #agent_executor.invoke("can you calculate the circumference of a circle that has a radius of 7.81mm")
-out = agent_executor.invoke({"input": "can you calculate the circumference of a circle that has a radius of 7.81mm", "chat_history": []})
+#out = agent_executor.invoke({"input": "can you calculate the circumference of a circle that has a radius of 7.81 mm and 10 mm", "chat_history": []})
+out = agent_executor.invoke({"input": "List all titles mentioned in the State of the union", "chat_history": []})
